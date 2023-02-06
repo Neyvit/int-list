867eb6421dc62df4178d3237aee2c5347ce93744
Neyvit <successfullproject@gmail.com>
2023-02-06 13:55:21 +0300

    Algorithms part3 - with grow() and resize() methods.

--- src/pro/sky/java/course2/IntListImpl.java
+++ src/pro/sky/java/course2/IntListImpl.java
@@ -13,14 +13,7 @@ public class IntListImpl implements IntList {
     private int[] intArray;
 
     public IntListImpl(int size) {
-        if (size == 100000) {
-            intArray = new int[100000];
-            for (int i = 0; i < intArray.length; i++) {
-                intArray[i] = (int) Math.round((Math.random() * 4294967295L) - 2147483647);
-            }
-        } else {
             intArray = new int[size];
-        }
     }
 
     public IntListImpl() {
@@ -29,10 +22,10 @@ public class IntListImpl implements IntList {
 
     @Override
     public boolean validateItem(int item) {
-        if (item < -2147483648 || 2147483647 < item) {
-            throw new InvalidItemRange();
-        } else {
+        if (item > -2147483648 && 2147483647 > item) {
             return true;
+        } else {
+            throw new InvalidItemRange();
         }
     }
 
@@ -46,26 +39,35 @@ public class IntListImpl implements IntList {
 
     @Override
     public int add(int item) {
-        if (size == intArray.length) {
-            throw new InvalidIndexException();
-        } else if (validateItem(item) == true) {
-            intArray[size++] = item;
-            return item;
+        if (size < intArray.length && validateItem(item)) {
+            intArray[size] = item;
+            size++;
+        } else if (size == intArray.length && validateItem(item)) {
+            grow();
+            intArray[size] = item;
+            size++;
         } else {
             throw new InvalidItemRange();
         }
+        return item;
     }
 
     @Override
     public int add(int index, int item) {
-        if (validateItem(item) == true && validateIndex(index) == true) {
-            for (int i = size; i >= index; i--) {
-                intArray[i + 1] = intArray[i];
-            }
+        if (size == intArray.length && validateItem(item)) {
+            grow();
+            System.arraycopy(intArray, index, intArray, index + 1, size - index);
             intArray[index] = item;
             size++;
+            return item;
+        } else if (validateItem(item) == true && size < intArray.length && size >= 0) {
+            System.arraycopy(intArray, index, intArray, index + 1, size - index);
+            intArray[index] = item;
+            size++;
+            return item;
+        } else {
+            throw new InvalidItemRange();
         }
-        return item;
     }
 
     @Override
@@ -88,17 +90,24 @@ public class IntListImpl implements IntList {
     public int remove(int index) {
         int temp = 0;
         if (validateIndex(index)) {
-            temp = intArray[index];
-            System.arraycopy(intArray, index + 1, intArray, index, size - index);
-            size--;
+            if (size < intArray.length / 2) {
+                temp = intArray[index];
+                System.arraycopy(intArray, index + 1, intArray, index, size - index);
+                size--;
+                resize();
+            } else {
+                temp = intArray[index];
+                System.arraycopy(intArray, index + 1, intArray, index, size - index);
+                size--;
+            }
         }
         return temp;
     }
 
     @Override
     public boolean contains(int item) {
-        int[] arr = toArray();
-        sort(arr);
+        int[] arr = intArray.clone();
+        quickSort(arr,0,size -1);
         return binarySearch(arr, item);
     }
 
@@ -151,10 +160,7 @@ public class IntListImpl implements IntList {
 
     @Override
     public boolean isEmpty() {
-        if (size == 0) {
-            return true;
-        }
-        return false;
+        return size == 0;
     }
 
     @Override
@@ -172,25 +178,40 @@ public class IntListImpl implements IntList {
         return Arrays.toString(intArray);
     }
 
-    private void swapElements(int[] intArray, int indexA, int indexB) {
-        int tmp = intArray[indexA];
-        intArray[indexA] = intArray[indexB];
-        intArray[indexB] = tmp;
+    private void swapElements(int[] intArray, int left, int right) {
+        int tmp = intArray[left];
+        intArray[left] = intArray[right];
+        intArray[right] = tmp;
     }
 
-    private void sort(int[] intArray) {
-        for (int i = 0; i < size - 1; i++) {
-            for (int j = 0; j < size - 1 - i; j++) {
-                if (intArray[j] > intArray[j + 1]) {
-                    swapElements(intArray, j, j + 1);
-                }
+    private int partition(int[] intArray, int begin, int end) {
+        int pivot = intArray[end];
+        int i = (begin - 1);
+
+        for (int j = begin; j < end; j++) {
+            if (intArray[j] <= pivot) {
+                i++;
+
+                swapElements(intArray, i, j);
             }
+        }
+
+        swapElements(intArray, i + 1, end);
+        return i + 1;
+    }
+
+    private void quickSort(int[] intArray, int begin, int end) {
+        if (begin < end) {
+            int partitionIndex = partition(intArray, begin, end);
+
+            quickSort(intArray, begin, partitionIndex - 1);
+            quickSort(intArray, partitionIndex + 1, end);
         }
     }
 
     private boolean binarySearch(int[] intArray, int element) {
         int min = 0;
-        int max = intArray.length - 1;
+        int max = size - 1;
 
         while (min <= max) {
             int mid = (min + max) / 2;
@@ -206,5 +227,17 @@ public class IntListImpl implements IntList {
             }
         }
         return false;
+    }
+
+    private void grow() {
+        int[] newArray = new int[intArray.length + intArray.length / 2];
+        System.arraycopy(intArray, 0, newArray, 0, size);
+        intArray = newArray;
+    }
+
+    private void resize() {
+        int[] newArray = new int[intArray.length - intArray.length / 3];
+        System.arraycopy(intArray, 0, newArray, 0, size);
+        intArray = newArray;
     }
 }
--- src/pro/sky/java/course2/Main.java
+++ src/pro/sky/java/course2/Main.java
@@ -26,10 +26,67 @@ public class Main {
         System.out.println(intArray.indexOf(1488));
         System.out.println(intArray.lastIndexOf(228));
         System.out.println(intArray.get(3));
+        System.out.println(intArray.add(1));
+        System.out.println(intArray.add(2));
+        System.out.println(intArray.add(3));
+        System.out.println(intArray.add(4));
+        System.out.println(intArray.add(5));
+        System.out.println(intArray.add(6));
+        System.out.println(intArray.add(7));
+        System.out.println(intArray.add(8));
+        System.out.println(intArray.add(9));
+        System.out.println(intArray.add(10));
+        System.out.println(intArray.add(11));
+        System.out.println(intArray.add(12));
+        System.out.println(intArray.add(13));
+        System.out.println(intArray.add(14));
+        System.out.println(intArray.toString());
+        System.out.println(intArray.add(15));
+        System.out.println(intArray.toString());
+        System.out.println(intArray.add(8, 55));
+        System.out.println(intArray.toString());
+        System.out.println(intArray.contains(228));
+        System.out.println(intArray.remove(5));
+        System.out.println(intArray.toString());
+        System.out.println(intArray.remove(5));
+        System.out.println(intArray.toString());
+        System.out.println(intArray.remove(5));
+        System.out.println(intArray.toString());
+        System.out.println(intArray.remove(5));
+        System.out.println(intArray.toString());
+        System.out.println(intArray.remove(5));
+        System.out.println(intArray.toString());
+        System.out.println(intArray.remove(5));
+        System.out.println(intArray.toString());
+        System.out.println(intArray.remove(5));
+        System.out.println(intArray.toString());
+        System.out.println(intArray.remove(5));
+        System.out.println(intArray.toString());
+        System.out.println(intArray.remove(5));
+        System.out.println(intArray.toString());
+        System.out.println(intArray.remove(5));
+        System.out.println(intArray.toString());
+        System.out.println(intArray.remove(5));
+        System.out.println(intArray.toString());
+        System.out.println(intArray.remove(5));
+        System.out.println(intArray.toString());
+        System.out.println(intArray.remove(5));
+        System.out.println(intArray.toString());
+        System.out.println(intArray.remove(5));
+        System.out.println(intArray.toString());
+        System.out.println(intArray.remove(5));
+        System.out.println(intArray.toString());
+        System.out.println(intArray.remove(5));
+        System.out.println(intArray.toString());
+        System.out.println(intArray.remove(5));
+        System.out.println(intArray.toString());
+
 
         int[] arr = generateRandomArray();
         int[] arr2 = arr.clone();
         int[] arr3 = arr.clone();
+        int[] arr4 = arr.clone();
+        int[] arr5 = arr.clone();
 
         long start = System.currentTimeMillis();
         sortBubble(arr);
@@ -37,25 +94,50 @@ public class Main {
 
         long start2 = System.currentTimeMillis();
         sortSelection(arr2);
-        System.out.println("SortSelectionTime " + (System.currentTimeMillis() - start));
+        System.out.println("SortSelectionTime " + (System.currentTimeMillis() - start2));
 
         long start3 = System.currentTimeMillis();
         sortInsertion(arr3);
-        System.out.println("SortInsertionTime " + (System.currentTimeMillis() - start));
+        System.out.println("SortInsertionTime " + (System.currentTimeMillis() - start3));
+
+        long start4 = System.currentTimeMillis();
+        quickSort(arr4, 0,arr4.length - 1);
+        System.out.println("QuickSortTime " + (System.currentTimeMillis() - start4));
+
+        long start5 = System.currentTimeMillis();
+        mergeSort(arr5);
+        System.out.println("MergeSortTime " + (System.currentTimeMillis() - start5));
     }
 
     public static int[] generateRandomArray() {
         int[] intArray = new int[100000];
+
         for (int i = 0; i < intArray.length; i++) {
             intArray[i] = (int) Math.round((Math.random() * 4294967295L) - 2147483647);
         }
         return intArray;
     }
 
-    private static void swapElements(int[] arr, int indexA, int indexB) {
-        int tmp = arr[indexA];
-        arr[indexA] = arr[indexB];
-        arr[indexB] = tmp;
+    private static void swapElements(int[] arr, int left, int right) {
+        int tmp = arr[left];
+        arr[left] = arr[right];
+        arr[right] = tmp;
+    }
+
+    private static int partition(int[] arr, int begin, int end) {
+        int pivot = arr[end];
+        int i = (begin - 1);
+
+        for (int j = begin; j < end; j++) {
+            if (arr[j] <= pivot) {
+                i++;
+
+                swapElements(arr, i, j);
+            }
+        }
+
+        swapElements(arr, i + 1, end);
+        return i + 1;
     }
 
     private static void sortBubble(int[] arr) {
@@ -89,6 +171,57 @@ public class Main {
                 j--;
             }
             arr[j] = temp;
+        }
+    }
+
+    public static void quickSort(int[] arr, int begin, int end) {
+        if (begin < end) {
+            int partitionIndex = partition(arr, begin, end);
+
+            quickSort(arr, begin, partitionIndex - 1);
+            quickSort(arr, partitionIndex + 1, end);
+        }
+    }
+
+    public static void mergeSort(int[] arr) {
+        if (arr.length < 2) {
+            return;
+        }
+        int mid = arr.length / 2;
+        int[] left = new int[mid];
+        int[] right = new int[arr.length - mid];
+
+        for (int i = 0; i < left.length; i++) {
+            left[i] = arr[i];
+        }
+
+        for (int i = 0; i < right.length; i++) {
+            right[i] = arr[mid + i];
+        }
+
+        mergeSort(left);
+        mergeSort(right);
+
+        merge(arr, left, right);
+    }
+
+    public static void merge(int[] arr, int[] left, int[] right) {
+
+        int mainP = 0;
+        int leftP = 0;
+        int rightP = 0;
+        while (leftP < left.length && rightP < right.length) {
+            if (left[leftP] <= right[rightP]) {
+                arr[mainP++] = left[leftP++];
+            } else {
+                arr[mainP++] = right[rightP++];
+            }
+        }
+        while (leftP < left.length) {
+            arr[mainP++] = left[leftP++];
+        }
+        while (rightP < right.length) {
+            arr[mainP++] = right[rightP++];
         }
     }
 }
